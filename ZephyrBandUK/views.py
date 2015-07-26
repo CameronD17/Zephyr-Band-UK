18ca22dfe483c74a59b0d3334fac05b72b7c6c33
CameronD17 <camerondoyle@live.co.uk>
2015-07-26 12:25:33 +0100

    Working version of Upcoming Gigs

--- ZephyrBandUK/views.py
+++ ZephyrBandUK/views.py
@@ -10,7 +10,7 @@ def onthecase(request):
 	venues = [x.text for x in soup.select('div.name a[href^=/venues]')]
 	venuelinks = ["http://www.onthecasemusic.co.uk" + x.attrs.get('href') for x in soup.select('div.name a[href^=/venues]')]
 	times = [x.text for x in soup.select('div.price')]		
-	gigs = zip(dates, venuelinks, venues, times)
+	gigs = zip(dates[:6], venuelinks[:6], venues[:6], times[:6])
 	t = loader.get_template('index.html')
 	html = t.render({'gig': gigs})	
 	return HttpResponse(html)
